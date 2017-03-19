aac40d0a5ac147279281f030dc1b695b4939c84b
Igornodari <igor_lealnodari@hotmail.com>
2017-03-19 13:00:37 -0300

    Add files via upload

---
+++ Kmporlitro.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kmporlitro;
+
+/**
+ *
+ * @author igorl
+ */
+public class Kmporlitro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    double distancia ;
+    distancia = 80.0;
+    double quantgas;
+    quantgas = 20.0;
+    double consumo;
+    consumo = (distancia/quantgas);
+    
+        System.out.println("valor do consumo "+consumo+ " km/litro");
+    
+    
+            }
+    
+    
+    
+           
+    
+}
