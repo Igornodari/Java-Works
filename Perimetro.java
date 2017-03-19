f5a6aec8dc1d0f20c85d762ed596917482d3092c
Igornodari <igor_lealnodari@hotmail.com>
2017-03-19 13:27:35 -0300

    Add files via upload

---
+++ Perimetro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perimetro;
+
+/**
+ *
+ * @author igorl
+ */
+public class Perimetro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     double cat1;
+     cat1 = 5.0;
+     double cat2;
+     cat2= 8.0;
+     double perimetro;
+     perimetro =(5.0+8.0);
+     System.out.println("O perimetro do triangulo retangulo é " + perimetro);
+             
+             
+     
+             
+    }
+    
+}
