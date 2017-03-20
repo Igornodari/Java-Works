cc6efd0290aa916b5cf9ac1f29ec7b882dee7c6b
Igornodari <igor_lealnodari@hotmail.com>
2017-03-20 20:59:10 -0300

    Add files via upload

---
+++ DobroDoNumero.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dobrodonumero;
+
+/**
+ *
+ * @author igorl
+ */
+public class DobroDoNumero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a;
+        a =(10*2);
+        System.out.println("O dobro da variavel A é = "+ a);
+        
+         
+        
+                
+    }
+    
+}
---
+++ Trocadevalores.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trocadevalores;
+
+/**
+ *
+ * @author igorl
+ */
+public class Trocadevalores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int x;
+    x = 10;
+    int y;
+    y = 20;
+    int k;
+    k = x;
+        System.out.println("valor de x = " + y);
+        System.out.println("valor de y = " + x);
+    
+   
+    
+   
+                     
+    
+           
+    }
+    
+}
