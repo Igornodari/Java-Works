955b6b5738d16b50ad9badecf7ae9d4a5df1300a
Igornodari <igor_lealnodari@hotmail.com>
2017-03-21 17:02:39 -0300

    Update Trocadevalores.java

--- Trocadevalores.java
+++ Trocadevalores.java
@@ -1,33 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trocadevalores;
 
-/**
- *
- * @author igorl
- */
 public class Trocadevalores {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-    int x;
-    x = 10;
-    int y;
-    y = 20;
-    int k;
-    k = x;
-        System.out.println("valor de x = " + y);
-        System.out.println("valor de y = " + x);
+public static void main(String[] args) {
     
+    int x = 10;
    
+    int y = 20;
     
-   
-                     
+    int k = x;
+    
+    x = y;
+    y = k;     
+        
+    System.out.println("valor de x = " + x);
+    System.out.println("valor de y = " + y);                     
     
            
     }
